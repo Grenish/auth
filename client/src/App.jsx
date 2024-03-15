3cd8b6291ef95786ab66466a07cd4bae344194fb
Grenish rai <ariestheracer@gmail.com>
2024-03-15 22:47:10 +0530

    Added Login and Signup components to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div >
+      {/* <Login /> */}
+      <Signup />
+    </div>
   )
 }
 
